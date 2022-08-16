7a38b2c8a764e915e165628050650725759ded84
Neeraj7979 <Imnsinha77@outlook.com>
2022-08-16 13:32:56 +0530

    method for displaying customer display

--- src/data/Customer.java
+++ src/data/Customer.java
@@ -6,19 +6,27 @@ public class Customer {
     private long mobileNumber;
     private String eMail;
     private boolean premiumMembership;
-    Customer(){
-        String name;
-        String address;
-        long mobileNumber;
-        String eMail;
-        boolean premiumMembership;
+    public Customer(){
+         name="";
+        address="";
+        mobileNumber=0;
+        eMail="";
+        premiumMembership=false;
     }
-    Customer(String name,String address,long mobileNumber,String eMail,boolean premiumMembership){
+    public Customer(String name,String address,long mobileNumber,String eMail,boolean premiumMembership){
         this.name=name;
         this.address=address;
         this.mobileNumber=mobileNumber;
         this.eMail=eMail;
         this.premiumMembership=premiumMembership;
+    }
+
+    public void display(){
+        System.out.println(" customer name = " + name);
+        System.out.println("address = " + address);
+        System.out.println("mobileNumber = " + mobileNumber);
+        System.out.println("eMail = " + eMail);
+        System.out.println("Is premium Member = " + premiumMembership);
     }
 
 
--- src/main/CustomerImpl.java
+++ src/main/CustomerImpl.java
@@ -1,4 +1,13 @@
 package main;
 
+import data.Customer;
+
 public class CustomerImpl {
+    public static void main(String[] args) {
+        Customer c1= new Customer("Sam","4th down street NewYork",7878765678L,"sam@gmail.com",
+                false);
+        Customer c2=new Customer();
+
+
+    }
 }

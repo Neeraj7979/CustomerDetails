301c998919707cd9e042525f91f51d7d6705f3a4
Neeraj7979 <Imnsinha77@outlook.com>
2022-08-16 13:14:38 +0530

    intial commit

---
+++ src/data/Customer.java
@@ -0,0 +1,11 @@
+package data;
+
+public class Customer {
+    private  String name;
+    private  String address;
+    private long mobileNumber;
+    private String eMail;
+    private boolean premiumMembership;
+
+
+}
